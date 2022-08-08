9a43b3b9a7e66e4bcbd2527ea7256b1de52408d4
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-08-08 10:35:57 +0300

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,20 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "main.h"
 
 /**
- * print_string - Prints a string
- * @list: list of arguments
- * Return: Will return the amount of characters printed.
+ * print_string - prints string
+ * @s: string to be printed
+ * Return: i
  */
-
-int print_string(va_list list)
+int print_string(va_list s)
 {
-	int i;
-	char *str;
-
-	str = va_arg(list, char *);
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+char *my_str;
+int i = 0;
+my_str = va_arg(s, char *);
+if (my_str == NULL)
+my_str = "(null)";
+while (my_str[i])
+{
+_putchar(my_str[i]);
+i++;
+}
+return (i);
 }
