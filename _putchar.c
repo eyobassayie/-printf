41d7c8a18f47fc57de103470367d80a62f4b3a65
eyob <85004717+eyobassayie@users.noreply.github.com>
2022-08-08 10:13:04 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -9,20 +9,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
-}
-
-
-/**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
- */
-int _puts(char *str)
-{
-	register short i;
-
-	for (i = 0; str[i]; i++)
-		_putchar(str[i]);
-	return (i);
+return (write(1, &c, 1));
 }
